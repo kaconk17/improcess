c2b4a4fbfdfcdd513615f2b077ec12716c99513e
Hartono <kaconk@Hartonos-MacBook-Pro.local>
2019-09-29 21:47:07 +0700

    import image with drag

--- improcess.py
+++ improcess.py
@@ -1,8 +1,12 @@
 import skimage.io as io
 from skimage.color import rgb2gray
-img = io.imread('sheep.jpg')
+gambar = input("Drag gambar kesini!")
+src = len(gambar)
+name = gambar[0:(src-4)]
+format = gambar[-4:]
+img = io.imread(gambar)
 img_grayscale = rgb2gray(img)
-io.imsave('sheep-gs.jpg', img_grayscale)
+io.imsave(name+'-gs'+format, img_grayscale)
 
 show_grayscale = io.imshow(img_grayscale)
 io.show()
